92673a5d72a316e5b507d7fd9bbe1bfb5763c25f
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2021-05-20 18:51:12 +0200

    Implement using functional approach by MA during live meeting

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -1,24 +1,31 @@
 #pragma once
 
+#include <functional>
+#include <map>
 #include <string>
 
-std::string calculate(const std::string& command, int first, int second) {
-    int result;
-    if (command == "add") {
-        result = first + second;
-    } else if (command == "subtract") {
-        result = first - second;
-    } else if (command == "multiply") {
-        result = first * second;
-    } else if (command == "divide") {
-        if (second) {
-            result = first / second;
-        } else {
-            return "Division by 0";
+std::map<std::string, std::function<std::string(int, int)>> commands {
+    { "add", [](int first, int second) {
+         return std::to_string(first + second);
+     } },
+    { "subtract", [](int first, int second) {
+         return std::to_string(first - second);
+     } },
+    { "multiply", [](int first, int second) {
+         return std::to_string(first * second);
+     } },
+    { "divide", [](int first, int second) {
+        if (second == 0) {
+            return std::string("Division by 0");
         }
-    } else {
+         return std::to_string(first / second);
+     } }
+};
+
+std::string calculate(const std::string& command, int first, int second) {
+    try {
+        return commands.at(command)(first, second);
+    } catch (...) {
         return "Invalid data";
     }
-    
-    return std::to_string(result);
 }
